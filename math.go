355deaeeb242bbd023badd2a48664e69ec3e20e5
Jayson Florez <jayson.florez@harborbytes.co>
2026-01-16 23:18:50 -0500

    second

--- math.go
+++ math.go
@@ -1,5 +1,9 @@
+// Package arithmetic provides a set of simple mathematical functio
 package arithmetic
 
+// Add takes two int values and returns the sum of them
+// See more at [MathIsFun]
+// [MathIsFun]: https://www.mathsisfun.com/numbers/addi
 func Add(num1, num2 int) int {
 	return num1 + num2
 }
