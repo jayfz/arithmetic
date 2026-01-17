e9830fc01c3455cdeb0d6634877816cf0bed1e58
Jayson Florez <jayson.florez@harborbytes.co>
2026-01-16 23:31:31 -0500

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jayfz/arithmetic
 
 go 1.25.5
+
+require golang.org/x/exp v0.0.0-20260112195511-716be5621a96
--- math.go
+++ math.go
@@ -1,9 +1,16 @@
 // Package arithmetic provides a set of simple mathematical functio
 package arithmetic
 
+import "golang.org/x/exp/constraints"
+
 // Add takes two int values and returns the sum of them
 // See more at [MathIsFun]
 // [MathIsFun]: https://www.mathsisfun.com/numbers/addi
-func Add(num1, num2 int) int {
+
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
+func Add[T Number](num1, num2 T) T {
 	return num1 + num2
 }
